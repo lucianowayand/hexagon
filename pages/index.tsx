4ea2c21784bd9864514e0b41860015bbd2ff7838
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2022-11-24 19:03:28 -0300

    feat: bringing back prisma

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,22 @@
-// import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react";
+import { ReactElement, JSXElementConstructor, ReactFragment, ReactPortal } from "react";
 
-// export async function getStaticProps() {
-// 	const res = await fetch(process.env.BASE_URL + "/api/attributes");
-// 	const data = await res.json();
+export async function getServerSideProps() {
+	const res = await fetch(process.env.BASE_URL + "/api/attributes");
+	const data = await res.json();
 
-// 	return {
-// 		props: { attributes: data },
-// 	};
-// }
+	return {
+		props: { attributes: data },
+	};
+}
 
 export default function Home({ attributes }: any) {
-	// const title = attributes.find((value: any) => value.name === "title").value;
-	// const primaryColor = attributes.find((value: any) => value.name === "primaryColor").value;
-	// const secondaryColor = attributes.find((value: any) => value.name === "secondaryColor").value;
+	const title = attributes.find((value: any) => value.name === "title").value;
+	const primaryColor = attributes.find((value: any) => value.name === "primaryColor").value;
+	const secondaryColor = attributes.find((value: any) => value.name === "secondaryColor").value;
 
 	return (
-		<div style={{height: "100vh", width: "100vw", padding: "1em" }}>
-			{/* <h1 style={{ color: secondaryColor }}>{title}</h1> */}
+		<div style={{height: "100vh", width: "100vw", padding: "1em", backgroundColor: primaryColor }}>
+			<h1 style={{color: secondaryColor}}>{title}</h1>
 		</div>
 	);
 }
