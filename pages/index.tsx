8ceb7df0f2ad035078f9b12b15972095c802536f
Luciano <lucianowayand@hotmail.com>
2022-11-25 09:21:07 -0300

    fix: wouldn't update if the database updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from "react";
 import HomeLayout from "../components/HomeLayout";
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
 	const res = await fetch(process.env.BASE_URL + "/api/attributes");
 	const data = await res.json();
 
@@ -12,9 +12,9 @@ export async function getStaticProps() {
 
 export default function Home({ attributes }: any) {
 	useEffect(() => {
-		localStorage.getItem("title") === null ? localStorage.setItem("title", attributes.find((value: any) => value.name === "title").value) : null;
-		localStorage.getItem("primaryColor") === null ? localStorage.setItem("primaryColor", attributes.find((value: any) => value.name === "primaryColor").value) : null;
-		localStorage.getItem("secondaryColor") === null ? localStorage.setItem("secondaryColor", attributes.find((value: any) => value.name === "secondaryColor").value) : null;
+		localStorage.setItem("title", attributes.find((value: any) => value.name === "title").value);
+		localStorage.setItem("primaryColor", attributes.find((value: any) => value.name === "primaryColor").value);
+		localStorage.setItem("secondaryColor", attributes.find((value: any) => value.name === "secondaryColor").value);
 	}, []);
 
 	return (
