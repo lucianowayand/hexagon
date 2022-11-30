4b10e302175cea6e5658910d015e3bda4036c5bd
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2022-11-30 12:40:46 -0300

    feat: added i18n at home page

--- components/HomeLayout/index.tsx
+++ components/HomeLayout/index.tsx
@@ -1,9 +1,14 @@
-import Link from "next/link";
 import React, { ReactNode, useContext } from "react";
-import { Context } from "../context/AttributesContext";
+import { Context } from "../../context/AttributesContext";
+
+import { In18 } from "./types";
+import i18n from './i18n.json'
+import Link from "next/link";
 
 export default function HomeLayout({ children }: { children: ReactNode }) {
-	const { title, primaryColor, secondaryColor } = useContext(Context)
+	const { title, primaryColor, secondaryColor, language } = useContext(Context)
+
+	const text = ((i18n as any)[language]) as In18
 
 	const linkStyle = {
 		margin: "1rem",
@@ -20,11 +25,14 @@ export default function HomeLayout({ children }: { children: ReactNode }) {
 					<h1>{title}</h1>
 				</Link>
 				<div style={{ display: "flex", flexDirection: "row" }}>
-					<Link href="/#" style={linkStyle}>
-						<h1>Crew</h1>
+					<Link href="#" style={linkStyle}>
+						<h1>{text.members}</h1>
 					</Link>
-					<Link href="/#" style={linkStyle}>
-						<h1>Projects</h1>
+					<Link href="#" style={linkStyle}>
+						<h1>{text.projects}</h1>
+					</Link>
+					<Link href="#" style={linkStyle}>
+						<h1>{text.sponsors}</h1>
 					</Link>
 				</div>
 			</div>
---
+++ components/HomeLayout/types.ts
@@ -0,0 +1,7 @@
+interface In18 {
+	members: string;
+	projects: string;
+	sponsors: string;
+}
+
+export type { In18 };
