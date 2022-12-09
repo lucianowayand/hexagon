81291a6337d9d2a85160ff46a8b97f375332effe
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2022-12-09 16:23:41 -0300

    feat: added analytics component

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -2,13 +2,17 @@ import type { AppProps } from "next/app";
 import ThemeProvider from "../context/ThemeContext";
 import AuthProvider from "../context/AuthContext";
 import "../styles/globals.css";
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
 	return (
-		<AuthProvider>
-			<ThemeProvider>
-				<Component {...pageProps} />
-			</ThemeProvider>
-		</AuthProvider>
+		<>
+			<AuthProvider>
+				<ThemeProvider>
+					<Component {...pageProps} />
+				</ThemeProvider>
+			</AuthProvider>
+			<Analytics />
+		</>
 	);
 }
