430cc94ee5068632240e9848cf7c89eeb8efab18
Luciano Wayand de Abreu <36968613+lucianowayand@users.noreply.github.com>
2022-12-09 15:12:46 -0300

    feat: made dashboard color dynamic

--- components/DashboardLayout/index.tsx
+++ components/DashboardLayout/index.tsx
@@ -2,20 +2,22 @@ import Link from "next/link";
 import { useRouter } from "next/router";
 import { useContext, useEffect } from "react"
 import { AuthContext } from "../../context/AuthContext";
+import { ThemeContext } from "../../context/ThemeContext";
 
 export default function DashboardLayout({ children }: { children: React.ReactNode }) {
     const { user, signOut } = useContext(AuthContext)
+    const { primaryColor, secondaryColor } = useContext(ThemeContext)
 
     const router = useRouter()
 
     useEffect(() => {
-        if (!user!.admin) {
-            router.push("/")
+        if (!user && user!.admin !== true) {
+            router.push("/login")
         }
     }, [user])
 
     return (
-        <div className="flex flex-row h-screen bg-gray-100">
+        <div className="flex flex-row h-screen" style={{background: primaryColor, color: secondaryColor}}>
             <div className="w-1/6 ml-5 mt-3 flex flex-col justify-between">
                 <div>
                     <div className="mb-6">
@@ -23,14 +25,14 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
                         <h1>{user?.email}</h1>
                     </div>
                     <ul>
-                        <li><Link href="/dashboard/users">Users</Link></li>
+                        <li><Link href="/dashboard/settings">Settings</Link></li>
                     </ul>
                 </div>
                 <div className="mb-7">
                     <a onClick={signOut}>Log out</a>
                 </div>
             </div>
-            <div className="w-5/6 px-10 py-6 bg-white">
+            <div className="w-5/6 px-10 py-6 bg-white" style={{color: "#000"}}>
                 {children}
             </div>
         </div>
